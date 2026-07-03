d6ece8dd91c9314646933980fa07c2d9c98812f1
Jason Li <lizhansong@pm.me>
2026-07-03 21:31:43 +1000

    build script

---
+++ build.ps1
@@ -0,0 +1,37 @@
+param (
+    [string]$Version
+)
+
+if (-Not (Test-Path ./venv))
+{
+    python -m venv venv
+}
+& ./venv/Scripts/activate.ps1
+
+python ./split.py
+
+if (Test-Path ./output) {
+    Remove-Item ./output -r -fo
+}
+New-Item -Name "output" -ItemType "Directory"
+New-Item -Path "./output" -Name "reframework" -ItemType "Directory"
+New-Item -Path "./output/reframework" -Name "fonts" -ItemType "Directory"
+New-Item -Path "./output/reframework" -Name "images" -ItemType "Directory"
+New-Item -Path "./output/reframework" -Name "autorun" -ItemType "Directory"
+
+$modinfo=@"
+name=RE2 Inventory Overlay
+version=v$version
+description=Inventory overlay for Resident Evil 2 (Ray Tracing update). Requires REFRamework
+author=NonBinarySearch
+screenshot=screenshot.png
+"@
+
+Out-File -FilePath "./output/modinfo.ini" -InputObject $modinfo
+Copy-Item "./screenshot.png" -Destination "./output/"
+
+Copy-Item "./RE2InventoryOverlayNonBinarySearch.lua" -Destination "./output/reframework/autorun"
+Copy-Item "./assets/NotoSansMono-SemiBold.ttf" -Destination "./output/reframework/fonts"
+Copy-Item "./assets/items" -Filter "*.png" -Destination "./output/reframework/images/NonBinarySearchInventoryOverlay/items/" -Recurse
+Copy-Item "./assets/weapons" -Filter "*.png" -Destination "./output/reframework/images/NonBinarySearchInventoryOverlay/weapons/" -Recurse
+
